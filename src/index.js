ee9330bccf7f75b321784bbdf9237165ffd62dc5
¨Chege <cochewaki@gmail.com>
2025-04-28 10:48:34 +0300

    provisional

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  e.preventDefault();
+	buildToDo(e.target.new_task_description.value);
 });
+
+function buildToDo(e_target_new_task_description_value) {
+	const list = document.createElement("li");
+	const button = document.createElement("button");
+
+	list.textContent = `${e_target_new_task_description_value}  `;
+
+	button.textContent = "x";
+
+	const unordered_list = document.getElementById("tasks");
+
+	unordered_list.appendChild(list);
+	list.appendChild(button);
+
+	button.addEventListener("click", deleteToDoItem);
+
+	function deleteToDoItem(list) {
+		return list.target.parentNode.remove();
+	}
+
+};
